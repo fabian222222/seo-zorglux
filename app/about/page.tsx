9c9b0f81d0cb246eefd8af46956ebf2224b68eb4
fabian222222 <fabian.zuo@supinternet.fr>
2024-05-24 05:04:26 +0200

    new: add new elements

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import React from 'react';
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -9,9 +10,14 @@ export default function About() {
         <meta name="description" content="En savoir plus sur Zorglux et notre passion pour le monde de Zorglub." />
       </Head>
       <main>
-      <h1>À propos de Zorglux</h1>
+        <h1>À propos de Zorglux</h1>
         <p>Chez Zorglux, nous sommes passionnés par Zorglub et ses aventures. Notre équipe se consacre à fournir des informations complètes et à jour sur ce personnage fascinant et son univers. Rejoignez-nous dans cette aventure incroyable!</p>
-        <img src="/images/about-zorglub.jpg" alt="À propos de Zorglub" />
+        <Image
+          src="/images/zorglub2.jpg"
+          width={500}
+          height={500}
+          alt="à propos de zorglub"
+        />
       </main>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,66 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Zorglux - L&apos;univers de Zorglub</title>
-        <meta name="description" content="En savoir plus sur Zorglux et notre passion pour le monde de Zorglub." />
-        <meta name="keywords" content="Zorglux, Zorglub, bande dessinée, à propos" />
+        <meta
+          name="description"
+          content="En savoir plus sur Zorglux et notre passion pour le monde de Zorglub."
+        />
+        <meta
+          name="keywords"
+          content="Zorglux, Zorglub, bande dessinée, à propos"
+        />
         <meta name="author" content="Zorglux Team" />
         <meta property="og:title" content="À propos de nous - Zorglux" />
-        <meta property="og:description" content="En savoir plus sur Zorglux et notre passion pour le monde de Zorglub." />
+        <meta
+          property="og:description"
+          content="En savoir plus sur Zorglux et notre passion pour le monde de Zorglub."
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://zorglux-zorglub.com/about" />
-        <meta property="og:image" content="https://zorglux-zorglub.com/images/og-image.jpg" />
+        <meta
+          property="og:image"
+          content="https://zorglux-zorglub.com/images/og-image.jpg"
+        />
       </Head>
       <main>
         <h1>Bienvenue chez Zorglux</h1>
-        <p>Découvrez Zorglux, votre destination ultime pour tout ce qui concerne Zorglub, l&apos;ennemi du héros Zurglux dans l&apos;anime célèbre. Plongez dans l&apos;univers fascinant de Zorglub, explorez ses aventures, et apprenez-en plus sur ce personnage emblématique.</p>
-        <img src="/images/zorglub-home.jpg" alt="Zorglub" />
+        <p>
+          Découvrez Zorglux, votre destination ultime pour tout ce qui concerne
+          Zorglub, l&apos;ennemi du héros Zurglux dans l&apos;anime célèbre.
+          Plongez dans l&apos;univers fascinant de Zorglub, explorez ses
+          aventures, et apprenez-en plus sur ce personnage emblématique.
+        </p>
+
+        <Image
+          src="/images/zorglub.jpg"
+          width={500}
+          height={500}
+          alt="Zorglub illustration"
+        />
         <section>
           <h2>Nos Dernières Aventures</h2>
           <ul>
-            <li><a href="/blog/zorglub-vs-zurglux">Zorglub vs Zurglux: Une Bataille Épique</a></li>
-            <li><a href="/blog/les-origines-de-zorglub">Les Origines de Zorglub</a></li>
-            <li><a href="/blog/les-armes-de-zorglub">Les Armes Secrètes de Zorglub</a></li>
+            <li>
+              <a href="/blog/zorglub-vs-zurglux">
+                Zorglub vs Zurglux: Une Bataille Épique
+              </a>
+            </li>
+            <li>
+              <a href="/blog/les-origines-de-zorglub">
+                Les Origines de Zorglub
+              </a>
+            </li>
+            <li>
+              <a href="/blog/les-armes-de-zorglub">
+                Les Armes Secrètes de Zorglub
+              </a>
+            </li>
           </ul>
         </section>
       </main>
